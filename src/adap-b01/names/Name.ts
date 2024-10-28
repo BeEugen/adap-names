c8f20ac5c1c8abcca7a3875b578744e778cfbb1c
Eugen Becker <becker.eugen91@gmail.com>
2024-10-28 12:41:49 +0100

    Add method type annotations in adap-b01 Name class

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -14,25 +14,30 @@ export class Name {
     }
 
     /** Returns human-readable representation of Name instance */
+    // @methodtype conversion-method
     public asNameString(delimiter: string = this.delimiter): string {
         return this.components.join(delimiter);
     }
 
+    // @methodtype get-method
     public getComponent(i: number): string {
         this.assertIsValidIndex(i);
         return this.components[i];
     }
 
+    // @methodtype set-method
     public setComponent(i: number, c: string): void {
         this.assertIsValidIndex(i);
         this.components[i] = c;
     }
 
     /** Returns number of components in Name instance */
+    // @methodtype get-method
     public getNoComponents(): number {
         return this.components.length;
     }
 
+    // @methodtype command-method
     public insert(i: number, c: string): void {
         if (i < 0 || i > this.components.length) {
             throw new Error("Index out of bounds");
@@ -40,19 +45,22 @@ export class Name {
         this.components.splice(i, 0, c);
     }
 
+    // @methodtype command-method
     public append(c: string): void {
         this.components.push(c);
     }
 
+    // @methodtype command-method
     public remove(i: number): void {
         this.assertIsValidIndex(i);
         this.components.splice(i, 1);
     }
 
+    // @methodtype assertion-method
     private assertIsValidIndex(i: number): void {
         if (i < 0 || i >= this.components.length) {
             throw new Error("Index out of bounds");
         }
     }
-    
+
 }
